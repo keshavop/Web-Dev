265b89b01cc9aee4790d01480c47fd496db09205
Keshav Kumar <keshavkumar8122@gmail.com>
2022-12-10 20:23:11 +0530

    js

---
+++ Javascript/forEachLoop.js
@@ -0,0 +1,13 @@
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// for each loop
+nums.forEach(element => {
+    console.log(element);
+});
+
+nums.forEach(function (ele) {
+    if (ele % 2 === 0) {
+        console.log(ele);
+    }
+}
+)
---
+++ Javascript/if_else.js
@@ -0,0 +1,9 @@
+let age;
+
+age = prompt("Enter age");
+
+if (age >= 18) {
+    console.log("You can vote");
+} else {
+    console.log("kal ana hee heee");
+}
